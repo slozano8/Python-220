eedf47049c0818bd57df83f23a9c259e6e8c8af7
Santos <95259821+slozano8@users.noreply.github.com>
2024-02-15 22:24:56 -0500

    Add files via upload

---
+++ Sdev220_MO4_ProgramingModules.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 15 08:34:03 2024
+
+@author: Santo
+"""
+
+#11.2 zoo.py file that with define function that prints open hours.
+import zoo 
+    
+
+
+print("our business hours are:")
+zoo.open_days()        
+
+
+import zoo as menagerie
+menagerie.hours_of_operations()
+
+zoo.holidays()
+
+
+   
+        
+if __name__=="__main__":
+    print()
+
+   
+       
+            
+            
+         
+        
+        
---
+++ zoo.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 15 20:51:32 2024
+
+@author: Santo
+"""
+
+def open_days():
+  ##prints the days of operations
+  print("monday,","tuesday,","wednesday,","thursday,","friday,", "saturday", "and sunday")
+
+def hours_of_operations():
+  ##Prints the zoo's opening hours with a different name.
+  print("9am - 5pm")
+  
+def holidays():
+    print("hours are subject to change during holidays")
+    
